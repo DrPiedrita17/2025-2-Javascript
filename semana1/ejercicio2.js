6452daca4253fc6f376028eb3f4fb0b9ddea54f7
DrPiedrita <chalchimorenow@gmail.com>
2025-05-07 02:44:51 -0600

    Resuelvo el ejercicio de la clase 2

--- semana1/ejercicio2.js
+++ semana1/ejercicio2.js
@@ -2,15 +2,20 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-  const age = Number(await ask('¿Cuántos años tienes? '));
-  const genere = await ask('¿Cuál es tu género? (hombre/mujer) ');
 
-  if ((age >= 1 && age < 18) && (genere === 'hombre')) {
-    console.log('Eres un hombre menor de edad');
-  } else if ((age >= 18 && age < 65) && (genere === 'hombre')) {
-    console.log('Eres un hombre mayor de edad');
-  } else {
-    console.log('No se cumplen los valores de búsqueda');
+  const age = Number(await ask ("¿Cuantos años tienes?"));
+  const genere = await ask("¿Cual es tu genero? (masculino/femenino)");
+
+  if(age <= 12 && genere === "masculino"){
+    console.log("Eres un hombre menor de edad");
+  } else if((age > 12 && age <= 18) && genere !== "femenino"){
+    console.log("Eres un hombre adolescente");
+  } else if((age > 18 && age <= 60) && genere === "masculino"){
+    console.log("Eres un hombre adulto");
+  } else if(age > 60 && genere === "masculino"){
+    console.log("Eres un hombre adulto mayor");
+  } else{
+    console.log("No se cumplen lños valores de busqueda");
   }
 }
 
